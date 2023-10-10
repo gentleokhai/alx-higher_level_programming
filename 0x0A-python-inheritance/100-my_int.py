fc832a9447f774613d6705bded4b800bbc79a4ad
gentleokhai <gentleokhaiX@gmail.com>
2023-10-09 20:07:40 -0700

    100

---
+++ 0x0A-python-inheritance/100-my_int.py
@@ -0,0 +1,14 @@
+#!/bin/usr/python3
+"""Defines a class that inherits from int"""
+
+
+class MyInt(int):
+    """And overides == and !="""
+
+    def __eq__(self, value):
+        """Overides == operator"""
+        return self.__value != value
+    
+    def __ne__(self, value):
+        """overides the != operator"""
+        return self.__value == value
