87cf384f089a19b0446e567250330d20658ff9aa
gentleokhai <gentleokhaiX@gmail.com>
2023-11-16 15:03:29 -0800

    h

---
+++ 0x0D-SQL_introduction/README.md
@@ -0,0 +1,1 @@
+SQL dealing with databases!!!
--- 0x18-dynamic_libraries/1-create_dynamic_lib.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-find . -name "*.c"
-for file in $(find . -name "*.c"); do
-    gcc -c -o ${file%.c}.o $file
-done
-gcc -shared -o liball.so *.o
